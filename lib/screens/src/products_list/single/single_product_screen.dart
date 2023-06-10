9905a2dced8b8611becf9ed796a5b9f0c8713159
slimpotatoboy <edwinmhzn@gmail.com>
2023-06-10 08:46:47 +0545

    single product error solved

--- lib/screens/src/products_list/single/single_product_screen.dart
+++ lib/screens/src/products_list/single/single_product_screen.dart
@@ -85,143 +85,154 @@ class _SingleProductScreenState extends State<SingleProductScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return products.businessId != storage.read('businessId')
-        ? InvalidWidget(
-            label: "Invalid QR - Scan Again",
-            onScan: () async {
-              var result = await BarcodeScanner.scan();
-              if (result.type.toString() != "Cancelled") {
-                Get.to(() => SingleProductScreen(result: result));
-              }
-            })
-        : RefreshIndicator(
-            onRefresh: onRefresh,
-            child: SingleScaffold(
-              title: !isLoading ? products.name : "",
-              child: isLoading
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Center(
-                            child: FutureBuilder(
-                              future: appwriteStorage.getFileView(
-                                bucketId: constants.productBucketId,
-                                fileId: products.thumbnail,
-                              ),
-                              builder: (context, snapshot) {
-                                return snapshot.hasData && snapshot.data != null
-                                    ? ClipRRect(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
-                                        child: Image.memory(
-                                          snapshot.data!,
-                                        ),
-                                      )
-                                    : const CircularProgressIndicator();
-                              },
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            "Rs. ${products.sellingPrice} /-",
-                            style: allRedTextStyle,
-                          ),
-                          const SizedBox(height: 10),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 8.0),
-                            child: SelectableText(
-                              "Cagtegory:  ${products.categoryName ?? 'N/A'}",
-                              style: lightStyle,
-                            ),
-                          ),
-                          const Divider(),
-                          const SizedBox(height: 10),
-                          const Text(
-                            "Description: ",
-                            style: boldStyle,
-                          ),
-                          const SizedBox(height: 5),
-                          SelectableText(
-                            products.description ?? "N/A",
-                            style: normalStyle,
-                          ),
-                          const SizedBox(height: 15),
-                          PrimaryButton(
-                              label: "Add to Cart",
-                              onPress: () {
-                                onAddToCart(context);
-                              }),
-                          const SizedBox(height: 25),
-                          if (products.businessId == storage.read('businessId'))
-                            Container(
-                              padding: const EdgeInsets.all(15.0),
-                              decoration: BoxDecoration(
-                                  color: greyColor,
-                                  border: Border.all(
-                                    width: 1,
-                                    color: darkGreyColor,
-                                  ),
-                                  borderRadius: BorderRadius.circular(10.0)),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text(
-                                    "Only Visible To You",
-                                    style: allRedSmallTextStyle,
-                                  ),
-                                  const Divider(),
-                                  Row(
-                                    children: [
-                                      Text("Status: "),
-                                      Chip(
-                                        label: Text(
-                                          products.status
-                                              ? "Public"
-                                              : "Private",
-                                        ),
-                                        side: BorderSide.none,
-                                        labelStyle: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12,
-                                        ),
-                                        backgroundColor: products.status
-                                            ? greenColor
-                                            : primaryColor,
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    "Supplier: ${products.supplierName ?? "N/A"}",
-                                    style: normalStyle,
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    "Cost Price: Rs. ${products.costPrice} /-",
-                                    style: normalStyle,
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    "Current Stock:  ${products.currentStock}",
-                                    style: normalStyle,
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    "Initial Stock: ${products.initialStock}",
-                                    style: normalStyle,
-                                  ),
-                                ],
+    if (isLoading) {
+      return const SingleScaffold(
+        title: "Loading",
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return products.businessId != storage.read('businessId')
+          ? InvalidWidget(
+              label: "Invalid QR - Scan Again",
+              onScan: () async {
+                var result = await BarcodeScanner.scan();
+                if (result.type.toString() != "Cancelled") {
+                  Get.to(() => SingleProductScreen(result: result));
+                }
+              })
+          : RefreshIndicator(
+              onRefresh: onRefresh,
+              child: SingleScaffold(
+                title: !isLoading ? products.name : "",
+                child: isLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: FutureBuilder(
+                                future: appwriteStorage.getFileView(
+                                  bucketId: constants.productBucketId,
+                                  fileId: products.thumbnail,
+                                ),
+                                builder: (context, snapshot) {
+                                  return snapshot.hasData &&
+                                          snapshot.data != null
+                                      ? ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          child: Image.memory(
+                                            snapshot.data!,
+                                          ),
+                                        )
+                                      : const CircularProgressIndicator();
+                                },
                               ),
                             ),
-                          const SizedBox(height: 25),
-                        ],
+                            const SizedBox(height: 20),
+                            Text(
+                              "Rs. ${products.sellingPrice} /-",
+                              style: allRedTextStyle,
+                            ),
+                            const SizedBox(height: 10),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: SelectableText(
+                                "Cagtegory:  ${products.categoryName ?? 'N/A'}",
+                                style: lightStyle,
+                              ),
+                            ),
+                            const Divider(),
+                            const SizedBox(height: 10),
+                            const Text(
+                              "Description: ",
+                              style: boldStyle,
+                            ),
+                            const SizedBox(height: 5),
+                            SelectableText(
+                              products.description ?? "N/A",
+                              style: normalStyle,
+                            ),
+                            const SizedBox(height: 15),
+                            PrimaryButton(
+                                label: "Add to Cart",
+                                onPress: () {
+                                  onAddToCart(context);
+                                }),
+                            const SizedBox(height: 25),
+                            if (products.businessId ==
+                                storage.read('businessId'))
+                              Container(
+                                padding: const EdgeInsets.all(15.0),
+                                decoration: BoxDecoration(
+                                    color: greyColor,
+                                    border: Border.all(
+                                      width: 1,
+                                      color: darkGreyColor,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      "Only Visible To You",
+                                      style: allRedSmallTextStyle,
+                                    ),
+                                    const Divider(),
+                                    Row(
+                                      children: [
+                                        Text("Status: "),
+                                        Chip(
+                                          label: Text(
+                                            products.status
+                                                ? "Public"
+                                                : "Private",
+                                          ),
+                                          side: BorderSide.none,
+                                          labelStyle: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 12,
+                                          ),
+                                          backgroundColor: products.status
+                                              ? greenColor
+                                              : primaryColor,
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      "Supplier: ${products.supplierName ?? "N/A"}",
+                                      style: normalStyle,
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      "Cost Price: Rs. ${products.costPrice} /-",
+                                      style: normalStyle,
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      "Current Stock:  ${products.currentStock}",
+                                      style: normalStyle,
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      "Initial Stock: ${products.initialStock}",
+                                      style: normalStyle,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            const SizedBox(height: 25),
+                          ],
+                        ),
                       ),
-                    ),
-            ),
-          );
+              ),
+            );
+    }
   }
 }
