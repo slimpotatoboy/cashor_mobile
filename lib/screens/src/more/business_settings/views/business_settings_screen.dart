dc5e78caea855daf8f6451b53ed8c63d3c7f4ab1
slimpotatoboy <edwinmhzn@gmail.com>
2023-06-10 08:41:24 +0545

    cmomented orders to book

--- lib/screens/src/more/business_settings/views/business_settings_screen.dart
+++ lib/screens/src/more/business_settings/views/business_settings_screen.dart
@@ -64,74 +64,70 @@ class _BusinessSettingsScreenState extends State<BusinessSettingsScreen> {
               Get.to(() => const BusinessProfileScreen());
             },
           ),
-          ListButton(
-            imageUrl: "user.svg",
-            label: "Orders to Books",
-            desc:
-                "Automatically generate cash for book sales from delivered and offline orders",
-            onPressed: () {
-              showModalBottomSheet(
-                context: context,
-                backgroundColor: Colors.white,
-                builder: (context) {
-                  return StatefulBuilder(
-                    builder: (BuildContext context, StateSetter setState) {
-                      return Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          color: whiteColor,
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        child: Wrap(
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.only(top: 20.0, bottom: 10.0),
-                              child: Text(
-                                "Orders to Book",
-                                style: boldStyle,
-                              ),
-                            ),
-                            Container(
-                              width: double.infinity,
-                              padding: const EdgeInsets.only(bottom: 25.0),
-                              child: const Text(
-                                "Automatically generate cash for book sales from delivered and offline orders",
-                                style: lightStyle,
-                              ),
-                            ),
-                            SwitchListTile(
-                              activeColor: whiteColor,
-                              activeTrackColor: greenColor,
-                              inactiveThumbColor: primaryColor,
-                              inactiveTrackColor: greyColor,
-                              title:
-                                  Text(!_businessStatus ? "Private" : "Public"),
-                              value: _businessStatus,
-                              onChanged: (bool value) async {
-                                final exe = await functions.createExecution(
-                                  functionId: constants.functionId,
-                                  data:
-                                      "{'databaseId': ${constants.appwriteDatabaseId}, 'collectionId': ${constants.ordersCollectionId}, 'businessId': '' }",
-                                );
-                                print(exe.response);
-                                // setState(() {
-                                //   _businessStatus = value;
-                                // });
-                                // storage.write("visibility", value);
-                                // updatePermissions(value);
-                              },
-                              secondary: const Icon(Icons.privacy_tip),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                },
-              );
-            },
-          ),
+          //TODO: next update
+          // ListButton(
+          //   imageUrl: "user.svg",
+          //   label: "Orders to Books",
+          //   desc:
+          //       "Automatically generate cash for book sales from delivered and offline orders",
+          //   onPressed: () {
+          //     showModalBottomSheet(
+          //       context: context,
+          //       backgroundColor: Colors.white,
+          //       builder: (context) {
+          //         return StatefulBuilder(
+          //           builder: (BuildContext context, StateSetter setState) {
+          //             return Container(
+          //               width: double.infinity,
+          //               padding: const EdgeInsets.all(20),
+          //               decoration: BoxDecoration(
+          //                 color: whiteColor,
+          //                 borderRadius: BorderRadius.circular(30.0),
+          //               ),
+          //               child: Wrap(
+          //                 children: [
+          //                   const Padding(
+          //                     padding: EdgeInsets.only(top: 20.0, bottom: 10.0),
+          //                     child: Text(
+          //                       "Orders to Book",
+          //                       style: boldStyle,
+          //                     ),
+          //                   ),
+          //                   Container(
+          //                     width: double.infinity,
+          //                     padding: const EdgeInsets.only(bottom: 25.0),
+          //                     child: const Text(
+          //                       "Automatically generate cash for book sales from delivered and offline orders",
+          //                       style: lightStyle,
+          //                     ),
+          //                   ),
+          //                   SwitchListTile(
+          //                     activeColor: whiteColor,
+          //                     activeTrackColor: greenColor,
+          //                     inactiveThumbColor: primaryColor,
+          //                     inactiveTrackColor: greyColor,
+          //                     title:
+          //                         Text(!_businessStatus ? "Private" : "Public"),
+          //                     value: _businessStatus,
+          //                     onChanged: (bool value) async {
+          //                       final exe = await functions.createExecution(
+          //                         functionId: constants.functionId,
+          //                         data:
+          //                             "{'databaseId': ${constants.appwriteDatabaseId}, 'collectionId': ${constants.ordersCollectionId}, 'businessId': '' }",
+          //                       );
+          //                       print(exe.response);
+          //                     },
+          //                     secondary: const Icon(Icons.privacy_tip),
+          //                   ),
+          //                 ],
+          //               ),
+          //             );
+          //           },
+          //         );
+          //       },
+          //     );
+          //   },
+          // ),
           ListButton(
             imageUrl: "key.svg",
             label: "Change Business Visibility",
